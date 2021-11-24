f97646a189681f1659c27b45b04f649b10ca2207
Takolander <william_tako@hotmail.com>
2021-11-24 14:25:04 +0100

    Grids
    Working with columns and rows in the grid tag. Created a 3x3 grid layout.

--- WPF 01/WPF 01/MainWindow.xaml.cs
+++ WPF 01/WPF 01/MainWindow.xaml.cs
@@ -24,30 +24,5 @@ namespace WPF_01
         {
             InitializeComponent();
         }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("Button was clicked - Direct event");
-        }
-
-        private void Button_MouseUp(object sender, MouseButtonEventArgs e)
-        {
-            MessageBox.Show("Mouse button went up / was released - Bubbling event");
-        }
-
-        private void Button_PreviewMouseUp(object sender, MouseButtonEventArgs e)
-        {
-            MessageBox.Show("Mouse button went up / was released - Tunneling event");
-        }
-
-        private void Button_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            MessageBox.Show("Left mouse button went down - Tunneling event");
-        }
-
-        private void Button_PreviewMouseRightButtonUp(object sender, MouseButtonEventArgs e)
-        {
-            MessageBox.Show("Right mouse button went up - Tunneling event");
-        }
     }
 }
