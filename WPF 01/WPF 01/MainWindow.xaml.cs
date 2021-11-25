210f31718e9c58233f0b1d91ce1c64549ddd435d
Takolander <william_tako@hotmail.com>
2021-11-25 14:30:32 +0100

    Password box
    Simple password box.

--- WPF 01/WPF 01/MainWindow.xaml.cs
+++ WPF 01/WPF 01/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace WPF_01
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Welcome " + tbUsername.Text);
+        }
     }
 }
