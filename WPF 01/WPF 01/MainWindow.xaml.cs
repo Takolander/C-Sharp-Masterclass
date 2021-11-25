e6b833032d0c16010587ad83f0bb63495d5cc721
Takolander <william_tako@hotmail.com>
2021-11-25 13:13:38 +0100

    ComboBox

--- WPF 01/WPF 01/MainWindow.xaml.cs
+++ WPF 01/WPF 01/MainWindow.xaml.cs
@@ -23,35 +23,8 @@ namespace WPF_01
         public MainWindow()
         {
             InitializeComponent();
-            List<Match> matches = new List<Match>();
-            matches.Add(new Match() { Team1 = "Bayern Munich", Team2 = "Real Madrid", Score1 = 3, Score2 = 2, Completion = 85 });
-            matches.Add(new Match() { Team1 = "PSG", Team2 = "Liverpool", Score1 = 1, Score2 = 2, Completion = 20 });
-            matches.Add(new Match() { Team1 = "AIK", Team2 = "DIF", Score1 = 1, Score2 = 3, Completion = 50 });
-            matches.Add(new Match() { Team1 = "Chelsea", Team2 = "AC Milan", Score1 = 0, Score2 = 0, Completion = 5 });
 
-            lbMatches.ItemsSource = matches;
+            comboBoxColors.ItemsSource = typeof(Colors).GetProperties();
         }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            if (lbMatches.SelectedItem != null)
-            {
-                MessageBox.Show("Selected Match: " + 
-                    (lbMatches.SelectedItem as Match).Team1 + " " + 
-                    (lbMatches.SelectedItem as Match).Score1 + ":" +
-                    (lbMatches.SelectedItem as Match).Score2 + " " +
-                    (lbMatches.SelectedItem as Match).Team2 + ", Completion: " +
-                    (lbMatches.SelectedItem as Match).Completion + " Minutes");
-            }
-        }
-    }
-
-    public class Match
-    {
-        public int Score1 { get; set; }
-        public int Score2 { get; set; }
-        public string Team1 { get; set; }
-        public string Team2 { get; set; }
-        public int Completion { get; set; }
     }
 }
