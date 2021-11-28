6b74b44ff161d0cdd3a10e09d068cb23f3311979
Takolander <william_tako@hotmail.com>
2021-11-28 17:10:59 +0100

    Linq with Lists
    Example of using Linq to filter students by gender

--- Linq1/Linq1/Program.cs
+++ Linq1/Linq1/Program.cs
@@ -10,26 +10,82 @@ namespace Linq1
     {
         static void Main(string[] args)
         {
-            int[] numbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            UniversityManager um = new UniversityManager();
 
-            OddNumbers(numbers);
+            um.MaleStudents();
+            um.FemaleStudents();
 
             Console.ReadKey();
         }
+    }
 
-        static void OddNumbers(int[] numbers)
+    class UniversityManager
+    {
+        public List<University> universities;
+        public List<Student> students;
+
+        public UniversityManager()
         {
-            Console.WriteLine("Odd Numbers: ");
+            universities = new List<University>();
+            students = new List<Student>();
 
-            // Using linq to filter array
-            IEnumerable<int> oddNumbers = from number in numbers where number % 2 != 0 select number;
+            universities.Add(new University { Id = 1, Name = "Yale" });
+            universities.Add(new University { Id = 2, Name = "Beijing Tech" });
 
-            Console.WriteLine(oddNumbers);
+            students.Add(new Student { Id = 1, Name = "Carla", Gender = "female", Age = 17, UniverityId = 1 });
+            students.Add(new Student { Id = 2, Name = "Toni", Gender = "male", Age = 21, UniverityId = 1 });
+            students.Add(new Student { Id = 3, Name = "Leyla", Gender = "female", Age = 19, UniverityId = 2 });
+            students.Add(new Student { Id = 4, Name = "James", Gender = "trans-gender", Age = 25, UniverityId = 2 });
+            students.Add(new Student { Id = 5, Name = "Linda", Gender = "female", Age = 22, UniverityId = 2 });
+        }
 
-            foreach  (int i in oddNumbers)
+        public void MaleStudents()
+        {
+            IEnumerable<Student> maleStudents = from student in students where student.Gender == "male" select student;
+            Console.WriteLine("Male - Students: ");
+
+            foreach (Student student in maleStudents)
             {
-                Console.WriteLine(i);
+                student.Print();
             }
+        }
+
+        public void FemaleStudents()
+        {
+            IEnumerable<Student> femaleStudents = from student in students where student.Gender == "female" select student;
+            Console.WriteLine("Female - Students: ");
+
+            foreach (Student student in femaleStudents)
+            {
+                student.Print();
+            }
+        }
+    }
+
+    class University
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        
+        public void Print()
+        {
+            Console.WriteLine("Univeristy {0} with Id {1}", Name, Id);
+        }
+    }
+
+    class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
+
+        // Foreign Key
+        public int UniverityId { get; set; }
+
+        public void Print()
+        {
+            Console.WriteLine("Student {0} with Id {1}, Gender {2} and age {3} from University with Id {4}", Name, Id, Gender, Age, UniverityId);
         }
     }
 }
