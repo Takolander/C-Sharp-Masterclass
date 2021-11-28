166440be7c0127a8e1e810104d6fec100d652c35
Takolander <william_tako@hotmail.com>
2021-11-28 21:05:20 +0100

    Inserting into SQL database
    Using Linq to insert data in the SQL database

--- LinqToSQL/LinqToSQL/MainWindow.xaml.cs
+++ LinqToSQL/LinqToSQL/MainWindow.xaml.cs
@@ -26,6 +26,44 @@ namespace LinqToSQL
 
             string connectionString = ConfigurationManager.ConnectionStrings["LinqToSQL.Properties.Settings.C_DBConnectionString"].ConnectionString;
             dataContext = new LinqToSQLDataClassesDataContext(connectionString);
+
+            //InsertUniversities();
+            InsertStudents();
+        }
+
+        public void InsertUniversities()
+        {
+            dataContext.ExecuteCommand("delete from University");
+
+            University yale = new University();
+            yale.Name = "Yale";
+            dataContext.Universities.InsertOnSubmit(yale);
+
+            University beijingTech = new University();
+            beijingTech.Name = "Beijing Tech";
+            dataContext.Universities.InsertOnSubmit(beijingTech);
+
+            dataContext.SubmitChanges();
+
+            MainDataGrid.ItemsSource = dataContext.Universities;
+        }
+
+        public void InsertStudents()
+        {
+            University yale = dataContext.Universities.First(un => un.Name.Equals("Yale"));
+            University beijingTech = dataContext.Universities.First(un => un.Name.Equals("Beijing Tech"));
+
+            List<Student> students = new List<Student>();
+
+            students.Add(new Student { Name = "Carla", Gender = "female", UniversityId = yale.Id });
+            students.Add(new Student { Name = "Tonie", Gender = "male", University = yale });
+            students.Add(new Student { Name = "Leyla", Gender = "female", University = beijingTech });
+            students.Add(new Student { Name = "James", Gender = "trans-gender", University = beijingTech });
+
+            dataContext.Students.InsertAllOnSubmit(students);
+            dataContext.SubmitChanges();
+
+            MainDataGrid.ItemsSource = dataContext.Students;
         }
     }
 }
