8bb329804a8b4b6b0389c246d27193b0a37ebf50
Takolander <william_tako@hotmail.com>
2021-11-30 00:33:39 +0100

    Static to API (CurrencyConverter_Static)
    Made the static currency converter use an API instead to get accurate values for conversion

--- CurrencyConverter_Static/CurrencyConverter_Static/MainWindow.xaml.cs
+++ CurrencyConverter_Static/CurrencyConverter_Static/MainWindow.xaml.cs
@@ -14,40 +14,114 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Data;
 using System.Text.RegularExpressions;
+using System.Net.Http;
+using Newtonsoft.Json;
 
 namespace CurrencyConverter_Static
 {
     public partial class MainWindow : Window
     {
+        Root val = new Root();
+
+        public class Root
+        {
+            public Rate rates { get; set; }
+            public long timestamp;
+            public string license;
+        }
+
+        public class Rate
+        {
+            public double SEK { get; set; }
+            public double GBP { get; set; }
+            public double INR { get; set; }
+            public double JPY { get; set; }
+            public double USD { get; set; }
+            public double NZD { get; set; }
+            public double EUR { get; set; }
+            public double CAD { get; set; }
+            public double CNY { get; set; }
+            public double ISK { get; set; }
+            public double RUB { get; set; }
+            public double NOK { get; set; }
+            public double PHP { get; set; }
+            public double DKK { get; set; }
+            public double CZK { get; set; }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+            GetValue();
+        }
+
+        private async void GetValue()
+        {
+            val = await GetData<Root>("https://openexchangerates.org/api/latest.json?app_id=2f0b7f5639b445e7b178e23024ce56d7");
             BindCurrency();
+        }
+
+        public static async Task<Root> GetData<T>(string url)
+        {
+            var myRoot = new Root();
+
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    client.Timeout = TimeSpan.FromMinutes(1);
+                    HttpResponseMessage response = await client.GetAsync(url);
+
+                    if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                    {
+                        var responseString = await response.Content.ReadAsStringAsync();
+                        var responseObject = JsonConvert.DeserializeObject<Root>(responseString);
+
+                        MessageBox.Show("TimeStamp: " + responseObject.timestamp, "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                        return responseObject;
+                    }
+                    return myRoot;
+                }
+            }
+            catch
+            {
+                return myRoot;
+            }
         }
 
         private void BindCurrency()
         {
-            DataTable dtCurrency = new DataTable();
+            DataTable dt = new DataTable();
 
             // Columns i datatable
-            dtCurrency.Columns.Add("Text");
-            dtCurrency.Columns.Add("Value");
+            dt.Columns.Add("Text");
+            dt.Columns.Add("Value");
 
             // Rows of the diffrent currencies
-            dtCurrency.Rows.Add("--SELECT--", 0);
-            dtCurrency.Rows.Add("INR", 1);
-            dtCurrency.Rows.Add("USD", 75);
-            dtCurrency.Rows.Add("EUR", 85);
-            dtCurrency.Rows.Add("SAR", 20);
-            dtCurrency.Rows.Add("POUND", 5);
-            dtCurrency.Rows.Add("DEM", 43);
-
-            cmbFromCurrency.ItemsSource = dtCurrency.DefaultView;
+            dt.Rows.Add("--SELECT--", 0);
+            dt.Rows.Add("SEK", val.rates.SEK);
+            dt.Rows.Add("GBP", val.rates.GBP);
+            dt.Rows.Add("INR", val.rates.INR);
+            dt.Rows.Add("JPY", val.rates.JPY);
+            dt.Rows.Add("USD", val.rates.USD);
+            dt.Rows.Add("NZD", val.rates.NZD);
+            dt.Rows.Add("EUR", val.rates.EUR);
+            dt.Rows.Add("CAD", val.rates.CAD);
+            dt.Rows.Add("CNY", val.rates.CNY);
+            dt.Rows.Add("ISK", val.rates.ISK);
+            dt.Rows.Add("RUB", val.rates.RUB);
+            dt.Rows.Add("NOK", val.rates.NOK);
+            dt.Rows.Add("PHP", val.rates.PHP);
+            dt.Rows.Add("DKK", val.rates.DKK);
+            dt.Rows.Add("CZK", val.rates.CZK);
+            
+            cmbFromCurrency.ItemsSource = dt.DefaultView;
             cmbFromCurrency.DisplayMemberPath = "Text";
             cmbFromCurrency.SelectedValuePath = "Value";
             cmbFromCurrency.SelectedIndex = 0;
 
-            cmbToCurrency.ItemsSource = dtCurrency.DefaultView;
+            cmbToCurrency.ItemsSource = dt.DefaultView;
             cmbToCurrency.DisplayMemberPath = "Text";
             cmbToCurrency.SelectedValuePath = "Value";
             cmbToCurrency.SelectedIndex = 0;
@@ -83,7 +157,7 @@ namespace CurrencyConverter_Static
             }
             else
             {
-                convertedValue = double.Parse(cmbFromCurrency.SelectedValue.ToString()) * double.Parse(txtCurrency.Text) / double.Parse(cmbToCurrency.SelectedValue.ToString());
+                convertedValue = double.Parse(cmbToCurrency.SelectedValue.ToString()) * double.Parse(txtCurrency.Text) / double.Parse(cmbFromCurrency.SelectedValue.ToString());
 
                 lblCurrency.Content = cmbToCurrency.Text + " " + convertedValue.ToString("N3");
             }
