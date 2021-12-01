9ca6cb208c0eb2f2bbfff55358e6017091e54be0
Takolander <william_tako@hotmail.com>
2021-12-01 10:19:10 +0100

    Thread Start and End & Completion
    Simple example of Starting a thread ending and seeing if it completed

--- Threads/Threads/Program.cs
+++ Threads/Threads/Program.cs
@@ -19,26 +19,41 @@ namespace Threads
             //Thread.Sleep(1000);
             //Console.WriteLine("Hello World! 4");
 
-            new Thread(() =>
+            //new Thread(() =>
+            //{
+            //    Thread.Sleep(1000);
+            //    Console.WriteLine("Thread 1");
+            //}).Start();
+            //new Thread(() =>
+            //{
+            //    Thread.Sleep(1000);
+            //    Console.WriteLine("Thread 2");
+            //}).Start();
+            //new Thread(() =>
+            //{
+            //    Thread.Sleep(1000);
+            //    Console.WriteLine("Thread 3");
+            //}).Start();
+            //new Thread(() =>
+            //{
+            //    Thread.Sleep(1000);
+            //    Console.WriteLine("Thread 4");
+            //}).Start();
+
+            var taskCompletionSource = new TaskCompletionSource<bool>();
+
+            var thread = new Thread(() =>
             {
-                Thread.Sleep(1000);
-                Console.WriteLine("Thread 1");
-            }).Start();
-            new Thread(() =>
-            {
-                Thread.Sleep(1000);
-                Console.WriteLine("Thread 2");
-            }).Start();
-            new Thread(() =>
-            {
-                Thread.Sleep(1000);
-                Console.WriteLine("Thread 3");
-            }).Start();
-            new Thread(() =>
-            {
-                Thread.Sleep(1000);
-                Console.WriteLine("Thread 4");
-            }).Start();
+                Console.WriteLine($"Thread numer: {Thread.CurrentThread.ManagedThreadId} started");
+                Thread.Sleep(5000);
+                taskCompletionSource.TrySetResult(true);
+                Console.WriteLine($"Thread numer: {Thread.CurrentThread.ManagedThreadId} ended");
+            });
+            
+            thread.Start();
+
+            var test = taskCompletionSource.Task.Result;
+            Console.WriteLine("task was done: {0}", test);
 
             Console.ReadLine();
         }
